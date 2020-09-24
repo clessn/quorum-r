b47ed4d0687b4e8daea53dd8db3a796d619f63cc
olichose123 <oli.banville@gmail.com>
2020-09-24 08:45:58 -0400

    adde html

---
+++ R/agoraplus.R
@@ -0,0 +1,95 @@
+#' @export
+createAgoraplusAuth <- function(username, password)
+{
+  return(httr::authenticate(username, password))
+}
+
+#' @export
+agoraplusLogin <- function()
+{
+  username <- readline(prompt='Username: ')
+  password <- readline(prompt='Password: ')
+  return(httr::authenticate(username, password))
+}
+
+#' @export
+createAgoraplusQuery <- function(source=NULL, tags=NULL)
+{
+  query = list()
+  if (!is.null(source))
+  {
+    query$source = source
+  }
+  if (!is.null(tags))
+  {
+    tags <- paste(tags, collapse=',')
+    query$tags = tags
+  }
+  return(query)
+}
+
+#' @export
+loadAgoraplusData <- function(query, auth, url='https://radarplus.clessn.com/article_html')
+{
+  cat('Loading data\n')
+  start_time <- Sys.time()
+  request <- httr::GET(url=url, config=auth, query=query)
+  if (request$status_code != 200)
+  {
+    stop(paste('request failed with a code', request$status_code))
+  }
+  content <- httr::content(request)
+  item_count <- content$count
+  next_page <- content$'next'
+  items <- content$results
+  data <- suppressWarnings(data.table::rbindlist(items))
+  cat(paste('found',item_count,'articles\n'))
+  current_count <- nrow(data)
+  while (current_count != item_count)
+  {
+    result <- loadPage(auth, next_page, data)
+    data <- result$data
+    next_page <- result$next_page
+    current_count = nrow(data)
+    cat(paste0('\r',current_count, '/', item_count, '...', percent(current_count/item_count), '        '))
+  }
+  
+  data$slug <- NULL
+  
+  for (col in colnames(data))
+  {
+    if (typeof(data[[col]]) == 'character')
+    {
+      Encoding(data[[col]]) <- "UTF-8"
+    }
+  }
+  
+  end_time <- Sys.time()
+  cat('\nsuccess with a ')
+  print(end_time - start_time)
+  return(data)
+}
+
+loadPage <- function(auth, url, data=NULL)
+{
+  request <- httr::GET(url=url, config=auth)
+  if (request$status_code != 200)
+  {
+    stop(paste('request failed with a code', request$status_code))
+  }
+  content <- httr::content(request)
+  items <- content$results
+  next_page <- content$'next'
+  
+  result <- suppressWarnings(data.table::rbindlist(items))
+  if (is.null(data))
+  {
+    data <- result
+  }
+  else
+  {
+    data <- rbind(data, result)
+  }
+  return(list(data=data, next_page=next_page))
+  
+}
--- example.R
+++ example.R
@@ -1,10 +1,12 @@
-library(quorum)
+##### DONNÉES RADARPLUS
 
-auth <- quorum::radarplusLogin()
+install.packages('devtools')
+devtools::install_github('clessn/quorum-r')
 
 begin_date <- quorum::createDate(1,7,2020, 00,00,00)
 end_date <- quorum::createDate(31,8,2020, 00,00,00)
 
+auth <- quorum::radarplusLogin()
 query <- quorum::createRadarplusQuery(begin_date=begin_date, end_date=end_date,
                              title_contains=c('covid'),
                              text_contains=c('covid'),
@@ -14,3 +16,10 @@ result <- quorum::loadRadarplusData(query, auth)
 
 
 
+##### DONNÉES AGORAPLUS
+
+install.packages('devtools')
+devtools::install_github('clessn/quorum-r')
+auth <- quorum::agoraplusLogin()
+query <- quorum::createAgoraplusQuery(tags=c('agoraplus'))
+result <- quorum::loadAgoraplusData(query, auth)
