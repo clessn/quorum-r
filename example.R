39e4276b86563b52665c53b0732bc1471e7936a7
olichose123 <oli.banville@gmail.com>
2020-10-23 13:15:30 -0400

    modified example

--- example.R
+++ example.R
@@ -6,28 +6,28 @@ devtools::install_github('clessn/quorum-r')
 
 
 # ======================================
-##### Récuperer les données Radar+ #####
+##### Rï¿½cuperer les donnï¿½es Radar+ #####
 
-# Se connecter au système. Dans la console, on vous demandera votre username et mot de passe
+# Se connecter au systï¿½me. Dans la console, on vous demandera votre username et mot de passe
 auth <- quorum::radarplusLogin()
 
-# on crée une requête avec les paramètres des articles qu'on veut
-begin_date <- quorum::createDate(1,7,2020, 00,00,00)
-end_date <- quorum::createDate(31,12,2020, 00,00,00)
+# on crï¿½e une requï¿½te avec les paramï¿½tres des articles qu'on veut
+begin_date <- quorum::createDate(1, 7, 2020, 00, 00, 00)
+end_date <- quorum::createDate(31, 12, 2020, 00, 00, 00)
 
 query <- quorum::createRadarplusQuery(
-    begin_date=begin_date, # les articles dont la fin de la une est après cette date
-    end_date=end_date,     # les articles dont la fin de la une est avant cette date
-    title_contains=c('covid', 'legault'),   # dont le titre contient covid ET legault (case ignorée)
-    text_contains=c('covid', 'québec'),     # dont le titre contient covid ET québec (case ignorée)
-    tags=c('quorum', 'french'),             # dont la source possède les tags quorum ET french
-    type='text'                             # choix: slug (info minime), text (texte seulement) ou html (html seulement)
+    begin_date = begin_date, # les articles dont la fin de la une est aprï¿½s cette date
+    end_date = end_date, # les articles dont la fin de la une est avant cette date
+    title_contains = c('covid', 'legault'), # dont le titre contient covid ET legault (case ignorï¿½e)
+    text_contains = c('covid', 'quï¿½bec'), # dont le titre contient covid ET quï¿½bec (case ignorï¿½e)
+    tags = c('quorum', 'french'), # dont la source possï¿½de les tags quorum ET french
+    type = 'text' # choix: slug (info minime), text (texte seulement) ou html (html seulement)
 )
 
-# On télécharge les données dans un data.table "result"
+# On tï¿½lï¿½charge les donnï¿½es dans un data.table "result"
 result <- quorum::loadRadarplusData(query, auth)
 
-# pour ajouter un texte traduit à un article
+# pour ajouter un texte traduit ï¿½ un article
 mon_slug <- result$slug[1]
 mon_text <- ''
 quorum::setRadarplusArticleTranslatedText(mon_slug, auth, mon_text)
@@ -36,24 +36,24 @@ quorum::setRadarplusArticleTranslatedText(mon_slug, auth, mon_text)
 
 
 # ======================================
-##### Récuperer les données Agora+ #####
+##### Rï¿½cuperer les donnï¿½es Agora+ #####
 
-# Même login que radarplus pour l'instant
+# Mï¿½me login que radarplus pour l'instant
 auth <- quorum::agoraplusLogin()
 
-# on crée une requête avec le tag agoraplus. on peut changer type='html' pour 'text' si on veut directement le texte extrait
-query <- quorum::createAgoraplusQuery(tags=c('agoraplus'), type='slug') # pour plus de contrôle, on peut générer une query avec createRadarplusQuery à la place
+# on crï¿½e une requï¿½te avec le tag agoraplus. on peut changer type='html' pour 'text' si on veut directement le texte extrait
+query <- quorum::createAgoraplusQuery(tags = c('agoraplus'), type = 'slug') # pour plus de contrï¿½le, on peut gï¿½nï¿½rer une query avec createRadarplusQuery ï¿½ la place
 result <- quorum::loadAgoraplusData(query, auth)
 
 
 
 # ======================================
-##### Publier, modifier et lister les données transformées Agora+ #####
+##### Publier, modifier et lister les donnï¿½es transformï¿½es Agora+ #####
 
-# Même login que radarplus pour l'instant
+# Mï¿½me login que radarplus pour l'instant
 auth <- quorum::agoraplusLogin()
 
-# Créer un nouvel objet
+# Crï¿½er un nouvel objet
 data <- list()
 data$ma_donnee_A <- "potato"
 data$ma_donnee_B <- "tomato"
@@ -75,4 +75,5 @@ result <- quorum::listAgoraplusTransformedData(auth)
 print(result)
 
 # Valider si un objet existe
-result <- quorum::getAgoraplusTransformedData(auth, 'mon_slug_de_donnee')
+result <- quorum::getAgoraplusTransformedData(auth, 'mon_slug_de_donnee2')
+result <- quorum::getAgoraplusTransformedData(auth, 'test')
